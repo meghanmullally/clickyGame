73f3ec3b246fdde95751036c86619a958277f52e
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-01 16:54:02 -0700

    changed background of wrapper, and changed background for navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <li className="navbar-brand">Clicky Game</li>
+      <nav className="navbar navbar-expand-lg navbar-white bg-white">
+        {/* <li className="navbar-brand">Clicky Game</li> */}
       <li className="score">Score:{props.score}
       </li>
       
