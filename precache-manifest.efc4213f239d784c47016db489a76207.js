b6c8985ce51ba2b5cd7c68dd88d19e464f1a7006
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-02 10:17:07 -0700

    Updates

---
+++ precache-manifest.efc4213f239d784c47016db489a76207.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b071c88292a871ecc02b94401404d",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "fb33d14449c9be3b81c6",
+    "url": "/clickyGame/static/css/main.afa0359b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fb33d14449c9be3b81c6",
+    "url": "/clickyGame/static/js/main.fa798bb3.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
