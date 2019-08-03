e679d4b4fd55d19352651f557bd5e4c4551ce761
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-03 12:06:21 -0700

    moved props.id value and handleClick into the card div

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -3,11 +3,10 @@ import "./style.css";
 
 function CharacterCard(props) {
   return (
-    <div className="card">
+    <div className="card" value={props.id} onClick={ () => props.handleClick(props.id) }>
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>  
-    <div className="click" value={props.id} onClick={ () => props.handleClick(props.id) }>x</div>
     </div>
   );
 }
